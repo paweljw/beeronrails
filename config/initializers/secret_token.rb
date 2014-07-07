536990b09cb365f36c5e07a2b269f9f6a17ad3fc
Paweł J. Wal <pjw@paweljw.eu>
2014-07-07 13:44:19 +0200

    Basic intro stuff, rails app setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beeronrails::Application.config.secret_token = '4a9ee1d23e869d85b51a201c6d723d0d0490d61443813e3c4e956ef022f6995466fc5b8fe302413662ab1d7766c8311f452d84a21a42ddffaa2f185572f2b823'
