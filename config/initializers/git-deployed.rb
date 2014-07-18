2df71a7981c22da6f93d52e51f82a6db34f91a64
Paweł J. Wal <pjw@paweljw.eu>
2014-07-18 15:25:29 +0200

    Fixing up travis, as per usual.

--- config/initializers/git-deployed.rb
+++ config/initializers/git-deployed.rb
@@ -3,4 +3,8 @@ include Grit
 repo = Repo.new(Rails.root)
 head = repo.commits.first
 
-Beeronrails::Application.config.gitid = head.id
+unless head.nil?
+	Beeronrails::Application.config.gitid = head.id
+else 
+	Beeronrails::Application.config.gitid = "travis"
+end
